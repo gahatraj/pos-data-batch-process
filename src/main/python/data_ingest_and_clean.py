2a4897488b8049b59aa34cc55e0ba40b9500b09d
Niranjan Gahatraj <niranjangahatraj1@gmail.com>
2021-09-28 14:56:39 -0400

    Added functionality to identify valid vs invalid data on combined past and current data

--- src/main/python/data_ingest_and_clean.py
+++ src/main/python/data_ingest_and_clean.py
@@ -39,13 +39,21 @@ s3_bucket_name = my_bucket
 current_date = datetime.now()
 # today_suffix = "_"+current_date.strftime("%d%m%Y")
 # yesterday_suffix = "_"+(current_date - timedelta(1)).strftime("%d%m%Y")
-pull_date_today = "_04062020"
-pull_date_yesterday = "_05062020"
+pull_date_yesterday = "_04062020"
+pull_date_today = "_05062020"
 
-# Read data from s3 bucket
-s3_bucket = s3.Object(bucket_name= s3_bucket_name, key=f'Sales_Landing/SalesDump{pull_date_today}/SalesDump.dat')
-response = s3_bucket.get()['Body'].read()
-sale_data = pd.read_csv(io.BytesIO(response), header=0, delimiter="|", low_memory=False)
+# Read past data from s3 bucket
+past_sale_data_bucket = s3.Object(bucket_name= s3_bucket_name, key=f'Sales_Landing/SalesDump{pull_date_yesterday}/SalesDump.dat')
+past_sale_data_response = past_sale_data_bucket.get()['Body'].read()
+past_sale_data = pd.read_csv(io.BytesIO(past_sale_data_response), header=0, delimiter="|", low_memory=False)
+
+
+# Read current data from s3 bucket
+current_sale_data_bucket = s3.Object(bucket_name= s3_bucket_name, key=f'Sales_Landing/SalesDump{pull_date_today}/SalesDump.dat')
+current_sale_data_response = current_sale_data_bucket.get()['Body'].read()
+current_sale_data = pd.read_csv(io.BytesIO(current_sale_data_response), header=0, delimiter="|", low_memory=False)
+
+
 
 # Sale_data Schema
 data_schema = StructType([
@@ -59,27 +67,106 @@ data_schema = StructType([
 ])
 
 # Unable to assign Timestamp type to Sale_Date, so creating temporary dataframe and updating column on the next step
-temp_df = spark.createDataFrame(sale_data, schema= data_schema)
+temp_past_sale_df = spark.createDataFrame(past_sale_data, schema= data_schema)
 
 # Update Sale_Date column with TimeStamp datatype
-final_df = temp_df.withColumn('Sale_Date', col('Sale_Date').cast("timestamp"))
+final_past_sale_df = temp_past_sale_df.withColumn('Sale_Date', col('Sale_Date').cast("timestamp"))
 
 # Register the DataFrame as a SQL temporary view
-final_df.createOrReplaceTempView("dailySaleData")
+final_past_sale_df.createOrReplaceTempView("pastSaleData")
 
-# Valid data
-valid_data = spark.sql("SELECT * FROM dailySaleData WHERE Quantity_Sold != 'NaN' AND Vendor_ID != 'NaN' ")
+
+
+# Unable to assign Timestamp type to Sale_Date, so creating temporary dataframe and updating column on the next step
+temp_current_sale_df = spark.createDataFrame(current_sale_data, schema= data_schema)
+
+# Update Sale_Date column with TimeStamp datatype
+final_current_sale_df = temp_current_sale_df.withColumn('Sale_Date', col('Sale_Date').cast("timestamp"))
+
+# Register the DataFrame as a SQL temporary view
+final_current_sale_df.createOrReplaceTempView("currentSaleData")
+
+# Combine past and current data and update the Quantity_Sold and Vendor_ID data
+joined_data = spark.sql("""
+                SELECT 
+                    CSD.Sale_ID,
+                    CSD.Product_ID,
+                    CASE
+                        WHEN CSD.Quantity_Sold = 'NaN' OR CSD.Quantity_Sold IS NULL THEN PSD.Quantity_Sold
+                        ELSE CSD.Quantity_Sold
+                    END AS Quantity_Sold,
+                    CASE 
+                        WHEN CSD.Vendor_ID = 'NaN' OR CSD.Vendor_ID IS NULL  THEN PSD.Vendor_ID
+                        ELSE CSD.Vendor_ID
+                    END AS Vendor_ID,
+                    CSD.Sale_Date,
+                    CSD.Sale_Amount,
+                    CSD.Sale_Currency
+                from currentSaleData AS CSD
+                LEFT OUTER JOIN pastSaleData AS PSD 
+                ON CSD.Sale_ID = PSD.Sale_ID
+""")
+joined_data.createOrReplaceTempView("joinedData")
+
+# Released hold data that are updated
+released_updated_record = spark.sql("""
+                SELECT 
+                    JD.Sale_ID
+                FROM joinedData AS JD
+                INNER JOIN pastSaleData AS PSD
+                ON JD.Sale_ID = PSD.Sale_ID
+
+""")
+released_updated_record.createOrReplaceTempView("releasedUpdatedRecord")
+
+# Held data that are still not updated
+held_record = spark.sql("""
+                SELECT * 
+                FROM pastSaleData 
+                WHERE Sale_ID NOT IN(SELECT 
+                                        Sale_ID  
+                                    FROM releasedUpdatedRecord
+                                )
+""")
+held_record.createOrReplaceTempView("heldPastSaleData")
 
 # Invalid data
-invalid_data = spark.sql("SELECT * FROM dailySaleData WHERE Quantity_Sold = 'NaN' OR Vendor_ID = 'NaN'")
+invalid_data = spark.sql("""
+                SELECT * 
+                FROM joinedData 
+                WHERE Quantity_Sold = 'NaN' OR Vendor_ID = 'NaN'
+                UNION
+                SELECT * 
+                FROM heldPastSaleData
+            """)
+invalid_data.show()
 
-
-def write_csv_on_s3(s3, content):
+# Valid data
+valid_data = spark.sql("""
+                SELECT * 
+                FROM joinedData 
+                WHERE Quantity_Sold != 'NaN' AND Vendor_ID != 'NaN' 
+""")
+valid_data.show()
+def save_validdata_to_s3(s3, valid_data):
     csv_buf = io.StringIO()
-    response = content.toPandas()
-    response.to_csv(csv_buf, header=True, index=False)
+    val_data = valid_data.toPandas()
+    val_data.to_csv(csv_buf, header=True, index=False)
     csv_buf.seek(0)
-    s3.Object('end-to-end-pipeline', 'output/valid/test.csv').put(Body=csv_buf.getvalue())
+    s3.Object('end-to-end-pipeline', f'output/valid/{pull_date_today}.csv').put(Body=csv_buf.getvalue())
+
+def save_invaliddata_to_s3(s3, invalid_data):
+    csv_buf = io.StringIO()
+    inval_data = invalid_data.toPandas()
+    inval_data.to_csv(csv_buf, header=True, index=False)
+    csv_buf.seek(0)
+    s3.Object('end-to-end-pipeline', f'output/invalid/{pull_date_today}.csv').put(Body=csv_buf.getvalue())
+
 
 # Send CSV file to S3 folder
-write_csv_on_s3(s3, valid_data)
+# save_validdata_to_s3(s3,valid_data)
+# save_invaliddata_to_s3(s3,invalid_data)
+
+
+
+
